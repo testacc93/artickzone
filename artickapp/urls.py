aad86d9da75f72c857f90991eb970ecdbd406244
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-03-11 12:20:58 +0530

    added pages and other things

--- artickapp/urls.py
+++ artickapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-
+    path('products/', views.products),
+    path('about/', views.about),
+    path('contact/', views.contact),
 ]
--- artickapp/views.py
+++ artickapp/views.py
@@ -4,3 +4,15 @@ from .models import Product
 def home(request):
     product = Product.objects.all()
     return render(request, 'home.html', context={'products':product})
+
+def products(request):
+    product = Product.objects.all()
+    return render(request, 'product.html', context={'products':product})
+
+def about(request):
+    product = Product.objects.all()
+    return render(request, 'about.html', context={'products':product})
+
+def contact(request):
+    product = Product.objects.all()
+    return render(request, 'contact.html', context={'products':product})
