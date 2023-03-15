fbde256e78360fc8c5b14ab5c201d51737b8ea5c
Ankur Kumar <bitcoinwallet02525@gmail.com>
2023-03-15 21:48:06 +0530

    added single product page

--- artickapp/urls.py
+++ artickapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('products/', views.products),
+    path('products/<str:prodname>', views.singleproduct),
+
     path('about/', views.about),
     path('contact/', views.contact),
 ]
--- artickapp/views.py
+++ artickapp/views.py
@@ -9,6 +9,10 @@ def products(request):
     product = Product.objects.all()
     return render(request, 'product.html', context={'products':product})
 
+def singleproduct(request,prodname):
+    product = Product.objects.get(name=prodname)
+    return render(request, 'singleprod.html', context={'product':product})
+
 def about(request):
     product = Product.objects.all()
     return render(request, 'about.html', context={'products':product})
